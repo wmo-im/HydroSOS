d3163ae2411a065e33436673042ee27293776d41
jbianchi81 <jbianchi@ina.gob.ar>
2025-05-20 13:34:21 -0300

    adapt to change in API: om-api

--- whos_client/get_test.sh
+++ whos_client/get_test.sh
@@ -1,10 +1,10 @@
 # output json to stdout
-python3 om_ogc_timeseries_client.py -t whos-36e7d1a3-b61e-4ae0-aca4-60efe6f27361 -m F426CC5C7D4A1563EE3B41E4F50FB4DE8568D3F2 -v B9240615C51DA438E9E02C9858887F1EE726EE2E 1990-01-01 2024-05-01
+python3 om_ogc_timeseries_client.py -t whos-36e7d1a3-b61e-4ae0-aca4-60efe6f27361 -m FAAC49BA633EFF325BE5D2BA81BE14574A268ABA -v Discharge 1990-01-01 2024-05-01
 # output to json file
-python3 om_ogc_timeseries_client.py -t whos-36e7d1a3-b61e-4ae0-aca4-60efe6f27361 -m F426CC5C7D4A1563EE3B41E4F50FB4DE8568D3F2 -v B9240615C51DA438E9E02C9858887F1EE726EE2E 1990-01-01 2024-05-01 -o data.json
+python3 om_ogc_timeseries_client.py -t whos-36e7d1a3-b61e-4ae0-aca4-60efe6f27361 -m FAAC49BA633EFF325BE5D2BA81BE14574A268ABA -v Discharge 1990-01-01 2024-05-01 -o data.json
 # output csv to stdout
-python3 om_ogc_timeseries_client.py -t whos-36e7d1a3-b61e-4ae0-aca4-60efe6f27361 -m F426CC5C7D4A1563EE3B41E4F50FB4DE8568D3F2 -v B9240615C51DA438E9E02C9858887F1EE726EE2E 1990-01-01 2024-05-01 -c
+python3 om_ogc_timeseries_client.py -t whos-36e7d1a3-b61e-4ae0-aca4-60efe6f27361 -m FAAC49BA633EFF325BE5D2BA81BE14574A268ABA -v Discharge 1990-01-01 2024-05-01 -c
 # output to csv file
-python3 om_ogc_timeseries_client.py -t whos-36e7d1a3-b61e-4ae0-aca4-60efe6f27361 -m F426CC5C7D4A1563EE3B41E4F50FB4DE8568D3F2 -v B9240615C51DA438E9E02C9858887F1EE726EE2E 1990-01-01 2024-05-01 -o data.csv -c
+python3 om_ogc_timeseries_client.py -t whos-36e7d1a3-b61e-4ae0-aca4-60efe6f27361 -m FAAC49BA633EFF325BE5D2BA81BE14574A268ABA -v Discharge 1990-01-01 2024-05-01 -o data.csv -c
 # retrieve using timeseries id instead of site id + variable id
-python3 om_ogc_timeseries_client.py -t whos-36e7d1a3-b61e-4ae0-aca4-60efe6f27361 -s F52452C81C0F06F1CAF659481D06817E9873AC91 1990-01-01 2024-05-01
+python3 om_ogc_timeseries_client.py -t whos-36e7d1a3-b61e-4ae0-aca4-60efe6f27361 -s 18EB307E3D1C45D3A2842D710A41001AB5083041 1990-01-01 2024-05-01
--- whos_client/om_ogc_timeseries_client.py
+++ whos_client/om_ogc_timeseries_client.py
@@ -24,7 +24,7 @@ class OmOgcTimeseriesClient:
     pagination_limit : int
 
     default_config : dict = {
-        "url": 'https://whos.geodab.eu/gs-service/services/essi',
+        "url": 'https://gs-service-preproduction.geodab.eu/gs-service/services/essi', # 'https://whos.geodab.eu/gs-service/services/essi',
         "token": 'MY_TOKEN',
         "timeseries_max": 48000,
         "timeseries_per_page": 400,
@@ -42,31 +42,68 @@ class OmOgcTimeseriesClient:
 
     def getTimeseries(
             self,
-            monitoringPoint : str = None,
-            timeseriesIdentifier : str = None,
+            feature : str = None,
+            observationIdentifier : str = None,
             beginPosition : str = None,
             endPosition : str = None,
             limit : int = None,
             offset : int = None,
             useCache : bool = False,
             view : str = None,
-            has_data : bool = False) -> dict:
-        
+            has_data : bool = False,
+            profiler : str = "om-api",
+            resumptionToken : str = None,
+            west : float = None, 
+            south : float = None, 
+            east : float = None, 
+            north : float = None,
+            ontology : str = None,
+            observedProperty : str = None,
+            timeInterpolation : str = None,
+            intendedObservationSpacing : str = None,
+            aggregationDuration : str = None,
+            includeData : bool = None,
+            asynchDownload : bool = None,
+            format : str = None) -> dict:
+
+        profiler_path =  "timeseries-api/timeseries" if profiler == "timeseries-api" else "om-api/observations"        
         view = view if view is not None else self.view
         limit = limit if limit is not None else self.pagination_limit
-        url = "%s/token/%s/view/%s/timeseries-api/timeseries" % (self.url, self.token, view)
+        url = "%s/token/%s/view/%s/%s" % (self.url, self.token, view, profiler_path)
         logging.debug("url: %s" % url)
-        response = requests.get(
-            url,
-            {
-                "monitoringPoint": monitoringPoint,
-                "timeseriesIdentifier": timeseriesIdentifier,
+        params = {
+                "monitoringPoint": feature,
+                "timeseriesIdentifier": observationIdentifier,
                 "beginPosition": beginPosition,
                 "endPosition": endPosition,
                 "limit": limit,
                 "offset": offset,
                 "useCache": useCache
-            })
+            } if profiler == "timeseries-api" else {
+                "feature": feature,
+                "observationIdentifier": observationIdentifier,
+                "beginPosition": beginPosition,
+                "endPosition": endPosition,
+                "east": east,
+                "south": south,
+                "west": west,
+                "north": north,
+                "observedProperty": observedProperty,
+                "ontology": ontology,
+                "timeInterpolation": timeInterpolation,
+                "intendedObservationSpacing": intendedObservationSpacing,
+                "aggregationDuration": aggregationDuration,
+                "includeData": includeData,
+                "asynchDownload": asynchDownload,
+                "useCache": useCache,
+                "limit": limit,
+                "useCache": useCache,
+                "resumptionToken": resumptionToken,
+                "format": format
+            } 
+        response = requests.get(
+            url,
+            params)
         if response.status_code != 200:
             raise ValueError("request failed, status code: %s, message: %s" % (response.status_code, response.text))
         result = response.json()
@@ -87,45 +124,64 @@ class OmOgcTimeseriesClient:
         self,
         beginPosition : str,
         endPosition : str,
-        monitoringPoint : str = None,
-        variableName : str = None,
-        timeseriesIdentifier : str = None,
-        view : str = None
-    ) -> List[dict]:
-        
-        if timeseriesIdentifier is None:
-            if monitoringPoint is None:
-                raise TypeError("monitoringPoint can't be None if timeseriesIdentifier is None")
-            if variableName is None:
-                raise TypeError("variableName can't be None if timeseriesIdentifier is None")
+        feature : str = None,
+        observedProperty : str = None,
+        observationIdentifier : str = None,
+        view : str = None,
+        timeInterpolation : str = None, # MAX, MIN, TOTAL, AVERAGE, MAX_PREC, MAX_SUCC, CONTINUOUS, ...
+        intendedObservationSpacing : str = None, # ISO8601 i.e. P1D
+        aggregationDuration : str = None, # ISO8601 i.e. P1D
+        ontology : str = None,
+        profiler : str = "om-api"
+    ) -> List[dict]:        
+        if observationIdentifier is None:
+            if feature is None:
+                raise TypeError("feature can't be None if timeseriesIdentifier is None")
+            if observedProperty is None:
+                raise TypeError("observedProperty can't be None if timeseriesIdentifier is None")
         view = view if view is not None else self.view
         
         # First, retrieves timeseries metadata for the monitoring point or timeseriesIdentifier
-        if timeseriesIdentifier is None:
+        if observationIdentifier is None:
             ts_metadata = self.getTimeseries(
                 view = view, 
-                monitoringPoint = monitoringPoint
+                feature = feature,
+                observedProperty = observedProperty,
+                timeInterpolation = timeInterpolation,
+                intendedObservationSpacing = intendedObservationSpacing,
+                aggregationDuration = aggregationDuration,
+                ontology = ontology,
+                profiler = profiler
             )
-            ts_matches = [x for x in ts_metadata["member"] if x["observedProperty"]["href"] == variableName]
-            if len(ts_matches):
-                timeseriesIdentifier = ts_matches[0]["id"]
+            if not len(ts_metadata["member"]):
+                raise FileNotFoundError("Observations not found for  monitoringPoint: %s, observedProperty: %s" % (feature, observedProperty))
+            if profiler == "timeseries-api":
+                ts_matches = [x for x in ts_metadata["member"] if x["observedProperty"]["href"] == observedProperty]
+                if len(ts_matches):
+                    observationIdentifier = ts_matches[0]["id"]
+                else:
+                    raise FileNotFoundError("timeseries metadata not found for monitoringPoint: %s, observedProperty: %s" % (feature, observedProperty))
             else:
-                raise ValueError("timeseries metadata not found for monitoringPoint: %s, variableName: %s" % (monitoringPoint, variableName))
+                if len(ts_metadata["member"]) > 1:
+                    logging.warning("Matched %i observations, retrieving first match" % len(ts_metadata["member"]))
+                observationIdentifier = ts_metadata["member"][0]["id"]
         else: 
             ts_metadata = self.getTimeseries(
                 view = view,
-                timeseriesIdentifier = timeseriesIdentifier
+                observationIdentifier = observationIdentifier,
+                profiler = profiler
             )
             if not len(ts_metadata):
-                raise ValueError("timeseries metadata not found for timeseriesIdentifier: %s" % timeseriesIdentifier)
+                raise ValueError("observation metadata not found for observationIdentifier: %s" % observationIdentifier)
             
         # Now that we have the timeseries identifier, retrieve data
         ts_data = self.getTimeseries(
             view = view,
-            # monitoringPoint = monitoringPoint,
             beginPosition = beginPosition,
             endPosition = endPosition,
-            timeseriesIdentifier = timeseriesIdentifier
+            observationIdentifier = observationIdentifier,
+            includeData = True,
+            profiler = profiler
         )
         return [ 
             {
@@ -155,9 +211,9 @@ def getData(token, url, output, csv, monitoring_point, variable_name, timeseries
     data = client.getData(
         begin_position, 
         end_position,
-        monitoringPoint = monitoring_point, 
-        variableName = variable_name, 
-        timeseriesIdentifier = timeseries_identifier
+        feature = monitoring_point, 
+        observedProperty = variable_name, 
+        observationIdentifier = timeseries_identifier
     )
     if output is not None:
         if csv:
